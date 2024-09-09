328df69d49ce081d236e612de4e1771db4a56e92
Nuraj250 <nurajshaminda200@gmail.com>
2024-09-08 22:31:15 -0600

    start

---
+++ src/test/java/com/example/city_Taxi/CityTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.city_Taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
