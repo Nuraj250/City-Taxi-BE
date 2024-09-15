a97d0464147ea8543ea7dce552354e190ab29cdf
Nuraj250 <nurajshaminda200@gmail.com>
2024-09-14 23:25:22 -0600

    User auth update and trip serch function

--- src/main/java/com/example/city_Taxi/repository/VehicleRepository.java
+++ src/main/java/com/example/city_Taxi/repository/VehicleRepository.java
@@ -1,7 +1,6 @@
 package com.example.city_Taxi.repository;
 
 import com.example.city_Taxi.model.Vehicle;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
