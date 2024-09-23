d77dbc66ea7a2718823c9aba7104576d4159bfd6
Nuraj250 <nurajshaminda200@gmail.com>
2024-09-23 15:50:16 -0600

    email init

--- src/main/java/com/example/city_Taxi/service/UserServiceImpl.java
+++ src/main/java/com/example/city_Taxi/service/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.example.city_Taxi.repository.UserRepository;
 import com.example.city_Taxi.util.Alert;
 import com.example.city_Taxi.util.ResponseMessage;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
