4ad928e8df13356278bd7eb4f4c400d1d750d6b0
Nuraj250 <nurajshaminda200@gmail.com>
2024-09-29 00:51:31 -0600

    promotion controller

--- src/main/java/com/example/city_Taxi/controller/PromotionController.java
+++ src/main/java/com/example/city_Taxi/controller/PromotionController.java
@@ -2,9 +2,13 @@ package com.example.city_Taxi.controller;
 
 import com.example.city_Taxi.model.Promotion;
 import com.example.city_Taxi.service.PromotionService;
+import com.example.city_Taxi.util.ResponseMessage;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 public class PromotionController {
 
@@ -18,24 +22,25 @@ public class PromotionController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getPromotionById(@PathVariable Long id) {
-        Optional<Promotion> promotion = promotionService.getPromotionById(id);
-        return promotion.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        ResponseMessage responseMessage = promotionService.getPromotionById(id);
+        return new ResponseEntity<>(responseMessage, HttpStatus.valueOf(responseMessage.getCode()));
     }
 
     @PostMapping
     public ResponseEntity<?> addPromotion(@RequestBody Promotion promotion) {
-        return ResponseEntity.ok(promotionService.addPromotion(promotion));
+        ResponseMessage responseMessage = promotionService.addPromotion(promotion);
+        return new ResponseEntity<>(responseMessage, HttpStatus.valueOf(responseMessage.getCode()));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updatePromotion(@PathVariable Long id, @RequestBody Promotion promotion) {
-//         updatedPromotion = promotionService.updatePromotion(id, promotion);
-//        return updatedPromotion != null ? ResponseEntity.ok(updatedPromotion) : ResponseEntity.notFound().build();
+        ResponseMessage responseMessage = promotionService.updatePromotion(id, promotion);
+        return new ResponseEntity<>(responseMessage, HttpStatus.valueOf(responseMessage.getCode()));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePromotion(@PathVariable Long id) {
-        promotionService.deletePromotion(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<?> deletePromotion(@PathVariable Long id) {
+        ResponseMessage responseMessage = promotionService.deletePromotion(id);
+        return new ResponseEntity<>(responseMessage, HttpStatus.valueOf(responseMessage.getCode()));
     }
 }
